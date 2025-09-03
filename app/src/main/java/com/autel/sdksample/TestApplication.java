be2a4ea956cb71acc581e93e8a32879f09dd6e43
Islam Abdymazhit <islam.abdymazhit@gmail.com>
2025-09-03 14:09:05 +0500

    Cleanup code

--- app/src/main/java/com/autel/sdksample/TestApplication.java
+++ app/src/main/java/com/autel/sdksample/TestApplication.java
@@ -17,15 +17,6 @@ import com.autel.sdk.Autel;
 import com.autel.sdk.AutelSdkConfig;
 import com.autel.sdk.product.BaseProduct;
 import com.autel.sdksample.evo.mission.util.AutelConfigManager;
-import com.autel.util.log.AutelLog;
-//import com.autel.xlog.LogConfiguration;
-//import com.autel.xlog.LogLevel;
-//import com.autel.xlog.printer.AndroidPrinter;
-//import com.autel.xlog.printer.XLogPrinter;
-//import com.autel.xlog.printer.file.FilePrinter;
-//import com.autel.xlog.printer.file.clean.FileLastModifiedCleanStrategy;
-//import com.autel.xlog.printer.file.naming.DateFileNameGenerator;
-//import com.autel.xlog.printer.flattener.ClassicFlattener;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,7 +33,6 @@ public class TestApplication extends Application {
 
     private static final long MAX_TIME = 1000 * 60 * 60 * 24 * 2; // two days
     private static  String LOG_PATH;
-//    public static XLogPrinter globalFilePrinter;
 
     public void onCreate() {
         super.onCreate();
@@ -181,33 +171,5 @@ public class TestApplication extends Application {
      */
     private void initXlog() {
         LOG_PATH = "sdcard/maxlink/";//AutelDirPathUtils.getLogCatPath();
-//        LogConfiguration config = new LogConfiguration.Builder().logLevel(BuildConfig.DEBUG ? LogLevel.ALL             // Specify log level, logs below this level won't be printed, default: LogLevel.ALL
-//                : LogLevel.NONE).tag(getString(R.string.app_name))                   // Specify TAG, default: "X-LOG"
-//                .t()                                                // Enable thread info, disabled by default
-//                .st(2)                                              // Enable stack trace info with depth 2, disabled by default
-//                .b()                                                // Enable border, disabled by default
-//        .addInterceptor(new BlacklistTagsFilterInterceptor(    // Add blacklist tags filter
-//            "blacklist1", "blacklist2", "blacklist3"))
-                // .addInterceptor(new WhitelistTagsFilterInterceptor( // Add whitelist tags filter
-                //     "whitelist1", "whitelist2", "whitelist3"))
-                // .addInterceptor(new MyInterceptor())                // Add a log interceptor
-//                .build();
-
-//        XLogPrinter androidPrinter = new AndroidPrinter();             // XLogPrinter that print the log using android.util.Log
-//        XLogPrinter filePrinter = new FilePrinter                      // XLogPrinter that print the log to the file system
-//                .Builder(new File(Environment.getExternalStorageDirectory(), LOG_PATH).getPath())       // Specify the path to save log file
-//                .fileNameGenerator(new DateFileNameGenerator())        // Default: ChangelessFileNameGenerator("log")
-//                // .backupStrategy(new MyBackupStrategy())             // Default: FileSizeBackupStrategy(1024 * 1024)
-//                .cleanStrategy(new FileLastModifiedCleanStrategy(MAX_TIME))     // Default: NeverCleanStrategy()
-//                .flattener(new ClassicFlattener())                     // Default: DefaultFlattener
-//                .build();
-
-//        AutelLog.init(                                                 // Initialize XLog
-//                config,                                                // Specify the log configuration, if not specified, will use new LogConfiguration.Builder().build()
-//                androidPrinter,                                        // Specify printers, if no printer is specified, AndroidPrinter(for Android)/ConsolePrinter(for java) will be used.
-//                filePrinter);
-
-        // For future usage: partial usage in MainActivity.
-//        globalFilePrinter = filePrinter;
     }
 }
